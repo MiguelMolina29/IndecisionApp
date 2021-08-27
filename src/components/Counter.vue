829c0363afcae137aba305f084cb055289d58d40
MiguelMolina29 <miguelantonio.molinago@my.uvm.edu.mx>
2021-08-27 17:43:19 -0500

    computed

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -1,6 +1,12 @@
 <template>
     <h2>Counter</h2>
-    <p> {{counter}} <sup>2</sup> = {{counter*counter}} </p>
+    <p> {{counter}} <sup>2</sup> = {{squareCounter}} </p>
+
+    <div>
+        <button @click="incrementar()">+1</button>
+        <button @click="decrementar()">-1</button>
+    </div>
+
 </template>
 
 <script>
@@ -10,9 +16,39 @@ export default {
             counter: 5,
         }
     },
+    methods: {
+        getSquareValue() {
+            return this.counter * this.counter
+        },
+        incrementar() {
+            return this.counter = this.counter + 1
+        },
+        decrementar() {
+            return this.counter = this.counter - 1
+        }
+    },
+    computed: {
+        squareCounter() {
+            return this.counter * this.counter
+        }
+    }
 }
 </script>
 
-<style>
+<style scoped>
+    button{ 
+        background-color: #64BB87;
+        border-radius: 5px;
+        border: 1px solid white;
+        color: white;
+        cursor: pointer;
+        margin: 0 5px;
+        padding: 5px 15px;
+        transition: 0.3s ease-in-out;
+    }
 
+    button:hover {
+        background-color: #5aa67b;
+        transition: 0.3s ease-in-out;
+    }
 </style>
