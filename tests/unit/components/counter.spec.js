9eed188e4f69e8f1d38178ed2f1e41e19b9b23d3
MiguelMolina29 <miguelantonio.molinago@my.uvm.edu.mx>
2021-09-08 21:29:29 -0500

    increase

--- tests/unit/components/counter.spec.js
+++ tests/unit/components/counter.spec.js
@@ -37,6 +37,29 @@ describe('Counter Component', () => {
         expect(pTags).toBe("100")
 
     })
+
+    test('Debe de incrementar y decrementar el contador', async() => {
+        
+        const wrapper = shallowMount( Counter )
+
+        const increaseBtn = wrapper.find('button')
+        
+        await increaseBtn.trigger('click') // trigger es un disparador
+        
+        let value = wrapper.find('[data-testid="counter"]').text()
+        
+        expect( value ).toBe('101')
+        
+        const decreaseBtn = wrapper.findAll('button')[1]
+
+        await decreaseBtn.trigger('click')
+        await decreaseBtn.trigger('click')
+
+        value = wrapper.find('[data-testid="counter"]').text()
+        expect( value ).toBe('99')
+
+    })
+    
     
     
 })
