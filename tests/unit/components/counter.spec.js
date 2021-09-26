9eae792c99930add855384c592774a0a9d42af05
MiguelMolina29 <miguelantonio.molinago@my.uvm.edu.mx>
2021-09-26 18:55:16 -0500

    refactor

--- tests/unit/components/counter.spec.js
+++ tests/unit/components/counter.spec.js
@@ -2,6 +2,12 @@ import {shallowMount} from '@vue/test-utils'
 import Counter from '@/components/Counter';
 
 describe('Counter Component', () => {
+
+    let wrapper = shallowMount( Counter );
+
+    beforeEach(() => {
+        wrapper = shallowMount( Counter )
+    })
     
     // test('Debe hacer match con el snapshot', () => {
         
@@ -14,7 +20,6 @@ describe('Counter Component', () => {
     // Segunda prueba
     test('H2 debe tener el valor por defecto de "Counter"', () => {
         
-        const wrapper = shallowMount( Counter );
 
         expect( wrapper.find('h2').exists() ).toBeTruthy()
 
@@ -26,8 +31,6 @@ describe('Counter Component', () => {
     
     test('valor por defecto debe ser 100 en el p', () => {
         
-        // Wrapper
-        const wrapper = shallowMount( Counter );
         // pTags
         // expect( wrapper.findAll('p').lastItem().toBeTruthy)
         const pTags = wrapper.find('[data-testid="counter"]').text()
@@ -40,23 +43,18 @@ describe('Counter Component', () => {
 
     test('Debe de incrementar y decrementar el contador', async() => {
         
-        const wrapper = shallowMount( Counter )
-
-        const increaseBtn = wrapper.find('button')
+        const [increaseBtn, decreaseBtn] = wrapper.findAll('button')
         
         await increaseBtn.trigger('click') // trigger es un disparador
+        await increaseBtn.trigger('click') // trigger es un disparador
+        await increaseBtn.trigger('click') // trigger es un disparador
         
-        let value = wrapper.find('[data-testid="counter"]').text()
-        
+
+        await decreaseBtn.trigger('click')
+        await decreaseBtn.trigger('click')
+
+        const value = wrapper.find('[data-testid="counter"]').text()
         expect( value ).toBe('101')
-        
-        const decreaseBtn = wrapper.findAll('button')[1]
-
-        await decreaseBtn.trigger('click')
-        await decreaseBtn.trigger('click')
-
-        value = wrapper.find('[data-testid="counter"]').text()
-        expect( value ).toBe('99')
 
     })
     
